95cf73da873c25d34e768ee299a303356c4fab25
upbitsrl <95976259+andreilungeanu@users.noreply.github.com>
2022-07-06 15:38:30 +0300

    Update

--- src/Twocheckout.php
+++ src/Twocheckout.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tisuchi\Twocheckout;
 
 abstract class Twocheckout
@@ -7,40 +8,34 @@ abstract class Twocheckout
     public static $privateKey;
     public static $username;
     public static $password;
-    public static $sandbox;
     public static $verifySSL = true;
     public static $baseUrl = 'https://www.2checkout.com';
     public static $error;
     public static $format = 'array';
-    const VERSION = '0.3.1';
+    const VERSION = '0.4.0';
 
-    public static function sellerId($value = null) {
+    public static function sellerId($value = null)
+    {
         self::$sid = $value;
     }
 
-    public static function privateKey($value = null) {
+    public static function privateKey($value = null)
+    {
         self::$privateKey = $value;
     }
 
-    public static function username($value = null) {
+    public static function username($value = null)
+    {
         self::$username = $value;
     }
 
-    public static function password($value = null) {
+    public static function password($value = null)
+    {
         self::$password = $value;
     }
 
-    public static function sandbox($value = null) {
-        if ($value == 1 || $value == true) {
-            self::$sandbox = true;
-            self::$baseUrl = 'https://sandbox.2checkout.com';
-        } else {
-            self::$sandbox = false;
-            self::$baseUrl = 'https://www.2checkout.com';
-        }
-    }
-
-    public static function verifySSL($value = null) {
+    public static function verifySSL($value = null)
+    {
         if ($value == 0 || $value == false) {
             self::$verifySSL = false;
         } else {
@@ -48,7 +43,8 @@ abstract class Twocheckout
         }
     }
 
-    public static function format($value = null) {
+    public static function format($value = null)
+    {
         self::$format = $value;
     }
 }
