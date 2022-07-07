2844c76d34e29d4d4294421e00bf667281bd595d
upbitsrl <95976259+andreilungeanu@users.noreply.github.com>
2022-07-07 14:15:11 +0300

    Create TwoCheckOut.php

---
+++ src/Facades/TwoCheckOut.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace TwoCheckOut\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class TwoCheckOut extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'Twocheckout';
+    }
+}
