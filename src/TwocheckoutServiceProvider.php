9f2ed5e222d898ec9d147441c4b844cf35bf1368
upbitsrl <95976259+andreilungeanu@users.noreply.github.com>
2022-07-07 12:45:49 +0300

    fix

---
+++ src/TwocheckoutServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Andreilungeanu\Twocheckout;
+use Illuminate\Support\ServiceProvider;
+
+class TwocheckoutServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
